763c352a20f3349260ce5d5c34e8641af52cab75
Artem Topolskyi <artem.t@mate.academy>
2026-02-14 13:13:09 +0200

    added BoostResource entity

---
+++ objects/resources/BoostResource/BoostResource.lua
@@ -0,0 +1,41 @@
+local BoostResource = GameObject:extend();
+
+function BoostResource:new(area, x, y, options)
+  BoostResource.super.new(self, area, 'BoostResource', x, y, options);
+
+  local screen_side_multiplier = ({ -1, 1 })[math.random(2)];
+
+  self.x = GAME_WIDTH / 2 + (screen_side_multiplier * (GAME_WIDTH / 2 + 48));
+  self.y = utils.random_float(48, GAME_HEIGHT - 48);
+
+  self.width, self.height = 12, 12;
+  self.horizontal_velocity = -screen_side_multiplier * utils.random_float(20, 40);
+
+  self:add_collider();
+
+  self.body:setLinearVelocity(self.horizontal_velocity, 0);
+  self.body:applyAngularImpulse(utils.random_float(-24, 24));
+end
+
+function BoostResource:update(dt)
+  BoostResource.super.update(self, dt);
+end
+
+function BoostResource:draw()
+  utils.push_rotate(self.x, self.y, self.body:getAngle());
+  love.graphics.setColor(COLOR.BOOST);
+  love.graphics.rectangle('line', self.x - self.width / 2, self.y - self.height / 2, self.width, self.height);
+  love.graphics.pop();
+end
+
+function BoostResource:add_collider()
+  self.body = love.physics.newBody(self.area.world, self.x, self.y, 'dynamic');
+  self.shape = love.physics.newRectangleShape(self.width, self.height);
+  self.fixture = love.physics.newFixture(self.body, self.shape);
+  self.fixture:setUserData(self);
+
+  self:setCollisionLayers(COLLISION_LAYER.PICKUP);
+  self:setCollisionMasks(COLLISION_LAYER.PLAYER);
+end
+
+return BoostResource;
